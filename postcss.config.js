2b5da92eaffa81f2cabd9449aa8ed8670abb0659
seanhillweb <seanhillweb@gmail.com>
2023-09-08 13:22:33 -0500

    Initial Commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/** @type {import("postcss").Config} */
+
+const postCSSConfig = {
+  plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+module.exports = postCSSConfig;
---
+++ src/app/global-error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+/**
+ * Summary. To specifically handle errors in root layout.js
+ *
+ * @link https://nextjs.org/docs/app/api-reference/file-conventions/error#global-errorjs
+ */
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
